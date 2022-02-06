b775f25e675c8fb759a61df11e4a626d08dca573
Vikalp Malhotra <kimboho@gmail.com>
2022-02-06 13:40:54 -0500

    commit tree 13e99dd292620a929819c5f55c7efec02b547e8c

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
+class CashRegister
+    attr_accessor :total
+    attr_accessor :price
+    def initialize
+        @total = 0 
+    end
 
+    # def total 
+    #     self.total 
+    # end
+    
+    # def add_item(title, price,quantity)
+    #     self.total += price
+
+    # end
+
+    # def apply_discount
+
+    #     self.total * ediscount
+    # end
+end
